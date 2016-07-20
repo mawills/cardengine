6ac601021764f10382bdfbb93ed1a2bde34e3044
Timofey Kondrashov <tkondrashov@gmail.com>
2016-07-20 03:07:49 +0100

    style cleanup for matt's changes

--- js/front/main.js
+++ js/front/main.js
@@ -1,12 +1,26 @@
-var mtg = new Game();
+var mtg = new Game(new UI());
 
 function update()
 {
+	$("#hand").empty();
 	var hand = mtg.getPlayer(P1).getHand();
 	for (var i=0; i<hand.length; i++)
 	{
-		$("#hand").append($("<img>",{ class: "card", src: 'res/img/cards/' + hand[i] + ".jpg"}));
+		(function(){
+			var card = hand[i];
+			$("#hand").append();
+		})();
 	}
+}
+
+function select(card)
+{
+
+}
+
+function getType(card)
+{
+
 }
 
 window.onload = function()
@@ -18,21 +32,15 @@ window.onload = function()
 // Toggle between Combat Log and Exile tabs on the UI
 function toggleLog(evt, tabName) {
 
-    var i, tabcontent, tablinks;
+	var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = $(".tabcontent").hide();
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = $(".tablinks").removeClass("active");
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.tabName += " active";
+	// Show the current tab, and add an "active" class to the link that opened the tab
+	$("#" + tabName).css("display", "block");
+	evt.currentTarget.tabName += " active";
 }
